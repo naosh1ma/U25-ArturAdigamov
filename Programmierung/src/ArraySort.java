f9df4a0585eecab96e148a7d17fc8d4781efc1dc
Arthur <grinshif@gmail.com>
2024-05-15 09:43:08 +0200

    -

--- Programmierung/src/ArraySort.java
+++ Programmierung/src/ArraySort.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class ArraySortV2 {
+public class ArraySort {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
