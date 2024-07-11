f4ff9d7fbf1306bf09fcde968f32ac3b52c07492
Arthur Adigamov <grinshif@gmail.com>
2024-07-11 09:32:52 +0200

    -

---
+++ src/RomanToArabicConverter/Main.java
@@ -0,0 +1,110 @@
+package RomanToArabicConverter;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String wahl, zahl;
+
+        do {
+            System.out.println("Befehlauswahl");
+            System.out.println("-------------");
+            System.out.println("Roemische eingeben  (r)");
+            System.out.println("Arabische eingeben  (a)");
+            System.out.println("Programm beenden    (x)");
+            wahl = scanner.nextLine();
+            System.out.print("Eingabe: ");
+            switch(wahl){
+                case "r": zahl = scanner.nextLine();
+                    System.out.println(zahl + ": " + roemArab(zahl));
+                    break;
+                case "a": zahl = scanner.nextLine();
+                    System.out.println(zahl + ": " + arabRoem(zahl));
+                    break;
+            }
+        }
+        while (!wahl.equals("x"));
+    }
+
+    public static String arabRoem(String zahl) {
+        StringBuilder roemzahl = new StringBuilder();
+        String ziffer = "";
+        int index = 0;
+        String[] f_einer = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        String[] f_zehner = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        String[] f_hunderter = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        String[] f_tausender = {"", "M", "MM", "MMM"};
+        if (zahl.length() > 4) {
+            return "Zahl zu lang!";
+        } else {
+            zahl = "000" + zahl;
+            zahl = zahl.substring(zahl.length() - 4);
+        }
+        for (int i = zahl.length(); i > 0; i--) {
+            ziffer = zahl.substring(i - 1, i);
+            index = Integer.parseInt(ziffer);
+            if (index != 0) {
+                switch (i) {
+                    case 1:
+                        roemzahl.insert(0, f_tausender[index]);
+                        break;
+                    case 2:
+                        roemzahl.insert(0, f_hunderter[index]);
+                        break;
+                    case 3:
+                        roemzahl.insert(0, f_zehner[index]);
+                        break;
+                    case 4:
+                        roemzahl.insert(0, f_einer[index]);
+                        break;
+                }
+            }
+        }
+        return roemzahl.toString();
+    }
+
+    public static String roemArab(String zahl) {
+        String zeichen = "";
+        int wert = 0, summe = 0, i;
+        zahl = pruefeZahl(zahl);
+        if (!zahl.isEmpty()) {
+            for (i = 0; i < zahl.length() - 1; i++) {
+                zeichen = String.valueOf(zahl.charAt(i));
+                wert = zahlenWert(zeichen);
+                if (wert < zahlenWert(String.valueOf(zahl.charAt(i + 1)))) {
+                    wert = wert * -1;
+                }
+                summe = summe + wert;
+            }
+            summe = summe + zahlenWert(String.valueOf(zahl.charAt(i)));
+        } else {
+            return "Fehler: Falsche Zahl";
+        }
+
+        return "" + summe;
+    }
+
+    public static int zahlenWert(String ziffer) {
+        int retVal = 0;
+        ziffer = ziffer.toUpperCase();
+        retVal = switch (ziffer) {
+            case "I" -> 1;
+            case "V" -> 5;
+            case "X" -> 10;
+            case "L" -> 50;
+            case "C" -> 100;
+            case "D" -> 500;
+            case "M" -> 1000;
+            default -> retVal;
+        };
+        return retVal;
+    }
+
+    public static String pruefeZahl(String zahl){
+
+        return "";
+    }
+}
