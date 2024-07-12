51fd57b667c07812968a26dbe7a7e5c9c514d2cd
Arthur Adigamov <grinshif@gmail.com>
2024-07-12 13:11:04 +0200

    -

--- src/NumberGuessingGame2/Zahlenraten.java
+++ src/NumberGuessingGame2/Zahlenraten.java
@@ -4,109 +4,91 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Zahlenraten {
-
+    // Variablen
     private Random random = null; // Zufallzahlen Generator
     private Scanner scanner = null;
     private int randomZahl;               // Zufallzahl
     private int minZahl, maxZahl;
-    private int minZ, maxZ;
     private int zahl;
-    private boolean fortsetzen = true;
-    private boolean gefunden = false;
 
     public Zahlenraten() {
         this.random = new Random();
         this.scanner = new Scanner(System.in);
     }
 
+    public Zahlenraten(int min, int max) {
+        this();
+        this.minZahl = min;
+        this.maxZahl = max;
+        sortiere_aufsteigend(min, max);
+    }
+
     // Schnittstellen-Methoden
     public int getMinZahl() {
         return minZahl;
     }
-
     public void setMinZahl(int minZahl) {
         this.minZahl = minZahl;
     }
-
     public int getMaxZahl() {
         return maxZahl;
     }
-
     public void setMaxZahl(int maxZahl) {
         this.maxZahl = maxZahl;
     }
-
-    public int getMinZ() {
-        return minZ;
-    }
-
-    public void setMinZ(int minZ) {
-        this.minZ = minZ;
-    }
-
-    public int getMaxZ() {
-        return maxZ;
-    }
-
-    public void setMaxZ(int maxZ) {
-        this.maxZ = maxZ;
-    }
-
     public int getZahl() {
         return zahl;
     }
-
     public void setZahl(int zahl) {
         this.zahl = zahl;
     }
-
     public int getRandomZahl() {
         return randomZahl;
     }
-
     public void setRandomZahl(int randomZahl) {
         this.randomZahl = randomZahl;
     }
 
+    // Sonstige Methoden
     public void start() {
-
         System.out.print("Bitte kleinste Zahl eingeben: ");
-        this.minZahl = scanner.nextInt();
+        this.minZahl = this.scanner.nextInt();
         System.out.print("Bitte groesste Zahl eingeben: ");
-        this.maxZahl = scanner.nextInt();
+        this.maxZahl = this.scanner.nextInt();
 
         sortiere_aufsteigend(this.minZahl, this.maxZahl);
-
     }
 
     public void sortiere_aufsteigend(int min, int max) {
+        int minZ, maxZ;
+        boolean fortsetzen = true;
+        boolean gefunden = false;
         do {
-            this.randomZahl = random.nextInt(min, max);
-            this.minZ = min;
-            this.maxZ = max;
-
+            this.randomZahl = this.random.nextInt(min, max); // zu erratende Zahl erzeugen und speichern
+            minZ = min;
+            maxZ = max;
             do {
-                System.out.println("Raten Sie Zahl zwischen " + this.minZ + " und " + this.maxZ);
-                this.zahl = scanner.nextInt();
-                if (this.randomZahl == this.zahl) {
-                    this.gefunden = true;
+                System.out.println("Raten Sie Zahl zwischen " + minZ + " und " + maxZ + "!");
+                setZahl(this.scanner.nextInt());
+                if (getRandomZahl() == getZahl()) {
+                    gefunden = true;
                 } else {
-                    if (this.zahl > this.randomZahl) {
-                        this.maxZ = this.zahl;
+                    if (getZahl() > getRandomZahl()) {
+                        maxZ = getZahl();
                     } else {
-                        this.minZ = this.zahl;
+                        minZ = getZahl();
                     }
                 }
             }
-            while (!this.gefunden);
+            while (!gefunden);
 
-            System.out.println("Die Zaufallzahl war: " + this.zahl);
+            System.out.println("Die Zaufallzahl war: " + getZahl() + "\n");
             System.out.println("Wollen Sie wieter spielen (j/n)?: ");
-            if (scanner.next().equals("n")) {
-                this.fortsetzen = false;
+            if (this.scanner.next().equals("n")) {
+                fortsetzen = false;
             } else {
-                this.gefunden = false;
+                gefunden = false;
             }
-        } while (this.fortsetzen);
+        } while (fortsetzen);
     }
 }
