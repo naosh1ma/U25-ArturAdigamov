c1280dd1546b41b13581cfa39a42984307dd1602
Arthur <grinshif@gmail.com>
2024-07-25 22:55:34 +0200

    -

--- src/Memory/GameModel.java
+++ src/Memory/GameModel.java
@@ -2,60 +2,77 @@ package Memory;
 
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class GameModel {
+
+
+    private int diff = 1;
     private JButton firstButton;
     private JButton secondButton;
-    private List<ImageIcon> icons;
-    private List<ImageIcon> iconsShaffled;
+    private ImageIcon[] icons;
+    private ImageIcon[] iconsShaffled;
 
-    private void createIcons(int diff) {
-        icons = new ArrayList<>();
-        iconsShaffled = new ArrayList<>();
+    GameModel() {
+        icons = new ImageIcon[64];
+    }
+    public int getDiff() {return diff;}
+    public void setDiff(int diff) {this.diff = diff;}
+
+    public void createIcons() {
         int index = 0;
-        switch (diff) {
+        switch (getDiff()) {
             case 1:
-                for (int i = 1; i <= 10; i++) {
-                    icons.add(index, new ImageIcon("icons\\sport" + i + "_64.png"));
-                    icons.add(index + 1, new ImageIcon("icons\\sport" + i + "_64.png"));
+                for (int i = 0; i < icons.length / 2; i++) {
+                    icons[i + index] = new ImageIcon("icons\\sport" + (i + 1) + "_128.png");
+                    icons[i + 1 + index] = new ImageIcon("icons\\sport" + (i + 1) + "_128.png");
                     index++;
                 }
-            case 2:
-                for (int i = 1; i <= 16; i++) {
-                    icons.add(index, new ImageIcon("icons\\sport" + i + "_64.png"));
-                    icons.add(index + 1, new ImageIcon("icons\\sport" + i + "_64.png"));
-                    index++;
-                }
-            case 3:
-                for (int i = 1; i <= 32; i++) {
-                    icons.add(index, new ImageIcon("icons\\sport" + i + "_64.png"));
-                    icons.add(index + 1, new ImageIcon("icons\\sport" + i + "_64.png"));
-                    index++;
-                }
-        }
-    }
-
-    public void iconsShuffled(int diff) {
-        switch (diff) {
-            case 1:
-                iconsShaffled = icons.subList(0, 20);
                 break;
             case 2:
-                iconsShaffled = icons.subList(0, 36);
+                for (int i = 0; i < icons.length / 2; i++) {
+                    icons[i + index] = new ImageIcon("icons\\sport" + (i + 1) + "_64.png");
+                    icons[i + 1 + index] = new ImageIcon("icons\\sport" + (i + 1) + "_64.png");
+                    index++;
+                }
                 break;
             case 3:
-                iconsShaffled = icons.subList(0, 64);
+                for (int i = 0; i < icons.length / 2; i++) {
+                    icons[i + index] = new ImageIcon("icons\\sport" + (i + 1) + "_32.png");
+                    icons[i + 1 + index] = new ImageIcon("icons\\sport" + (i + 1) + "_32.png");
+                    index++;
+                }
                 break;
         }
-        Collections.shuffle(iconsShaffled);
+    }
+    public void iconsShuffle() {
+        switch (getDiff()) {
+            case 1:
+                iconsShaffled = new ImageIcon[20];
+                for (int i = 0; i < iconsShaffled.length; i++) {
+                    iconsShaffled[i] = icons[i];
+                }
+                break;
+            case 2:
+                iconsShaffled = new ImageIcon[36];
+                for (int i = 0; i < iconsShaffled.length; i++) {
+                    iconsShaffled[i] = icons[i];
+                }
+                break;
+            case 3:
+                iconsShaffled = new ImageIcon[64];
+                for (int i = 0; i < iconsShaffled.length; i++) {
+                    iconsShaffled[i] = icons[i];
+                }
+                break;
+        }
+        Collections.shuffle(Arrays.asList(iconsShaffled));
     }
 
-    public ImageIcon getIcon(int diff, int index) {
-        createIcons(diff);
-        iconsShuffled(diff);
-        return iconsShaffled.get(index);
+    public ImageIcon getIcon(int index) {
+        return iconsShaffled[index];
     }
 }
 
--- src/Memory/GameView.java
+++ src/Memory/GameView.java
@@ -56,21 +56,24 @@ public class GameView extends JFrame {
 
     public GameView(GameModel model) {
 
+        /*
+
+        add(panelWelcome);
+        contentPane.setVisability();
+        */
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 650, 450);
+        setBounds(100, 100, 750, 600);
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
         contentPane.setLayout(null);
         welcome();
-        contentPane.add(panelWelcome);
-
     }
 
     private void welcome() {
         panelWelcome = new JPanel();
-        panelWelcome.setBounds(0, 0, 634, 411);
+        panelWelcome.setBounds(0, 0, 734, 561);
         panelWelcome.setBorder(new EmptyBorder(5, 5, 5, 5));
         panelWelcome.setLayout(null);
 
@@ -84,10 +87,6 @@ public class GameView extends JFrame {
         btnGameEasy.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 startEasy();
-                getContentPane().removeAll();
-                getContentPane().add(panelGameEasy);
-                getContentPane().revalidate();
-                getContentPane().repaint();
             }
         });
         btnGameEasy.setFont(new Font("Arial", Font.PLAIN, 15));
@@ -98,10 +97,6 @@ public class GameView extends JFrame {
         btnGameNorm.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 startNorm();
-                getContentPane().removeAll();
-                getContentPane().add(panelGameNorm);
-                getContentPane().revalidate();
-                getContentPane().repaint();
             }
         });
         btnGameNorm.setFont(new Font("Arial", Font.PLAIN, 15));
@@ -112,10 +107,6 @@ public class GameView extends JFrame {
         btnGameHard.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 startHard();
-                getContentPane().removeAll();
-                getContentPane().add(panelGameHard);
-                getContentPane().revalidate();
-                getContentPane().repaint();
             }
         });
         btnGameHard.setFont(new Font("Arial", Font.PLAIN, 15));
@@ -140,23 +131,31 @@ public class GameView extends JFrame {
         btnRangList.setFont(new Font("Arial", Font.PLAIN, 15));
         btnRangList.setBounds(243, 332, 95, 45);
         panelWelcome.add(btnRangList);
+
+        getContentPane().removeAll();
+        getContentPane().add(panelWelcome);
+        getContentPane().revalidate();
+        getContentPane().repaint();
     }
 
     private void startEasy() {
         panelGameEasy = new JPanel();
         panelGameEasyField = new JPanel();
-        panelGameEasy.setBounds(0, 0, 634, 411);
-        panelGameEasyField.setBounds(0, 1, 634, 350);
+        panelGameEasy.setBounds(0, 0, 734, 561);
+        panelGameEasyField.setBounds(0, 1, 734, 503);
         panelGameEasy.setLayout(null);
         panelGameEasyField.setLayout(new GridLayout(ROW_SIZE, COLUMN_SIZE));
         panelGameEasy.add(panelGameEasyField);
         buttonsGameEasy = new JButton[ROW_SIZE][COLUMN_SIZE];
         int index = 0;
         GameModel model = new GameModel();
+        model.setDiff(1);
+        model.createIcons();
+        model.iconsShuffle();
         for (int i = 0; i < ROW_SIZE; i++) {
             for (int j = 0; j < COLUMN_SIZE; j++) {
                 buttonsGameEasy[i][j] = new JButton();
-                buttonsGameEasy[i][j].setIcon(model.getIcon(1,index));
+                buttonsGameEasy[i][j].setIcon(model.getIcon(index));
                 panelGameEasyField.add(buttonsGameEasy[i][j]);
                 index++;
             }
@@ -166,40 +165,49 @@ public class GameView extends JFrame {
         btnGameEasyEnd.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 welcome();
-                getContentPane().removeAll();
-                getContentPane().add(panelWelcome);
-                getContentPane().revalidate();
-                getContentPane().repaint();
             }
         });
         btnGameEasyEnd.setFont(new Font("Arial", Font.PLAIN, 15));
-        btnGameEasyEnd.setBounds(517, 362, 105, 35);
+        btnGameEasyEnd.setBounds(607, 515, 105, 35);
         panelGameEasy.add(btnGameEasyEnd);
 
         btnGameEasyNewStart = new JButton("Neustarten");
         btnGameEasyNewStart.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                startEasy();
             }
         });
         btnGameEasyNewStart.setFont(new Font("Arial", Font.PLAIN, 15));
-        btnGameEasyNewStart.setBounds(389, 362, 105, 35);
+        btnGameEasyNewStart.setBounds(479, 515, 105, 35);
         panelGameEasy.add(btnGameEasyNewStart);
+
+        getContentPane().removeAll();
+        getContentPane().add(panelGameEasy);
+        getContentPane().revalidate();
+        getContentPane().repaint();
     }
 
     private void startNorm() {
         panelGameNorm = new JPanel();
         panelGameNormField = new JPanel();
-        panelGameNorm.setBounds(0, 0, 634, 411);
-        panelGameNormField.setBounds(0, 1, 634, 350);
+        panelGameNorm.setBounds(0, 0, 734, 561);
+        panelGameNormField.setBounds(0, 1, 734, 503);
         panelGameNorm.setLayout(null);
         panelGameNormField.setLayout(new GridLayout(NORM_SIZE, NORM_SIZE));
         panelGameNorm.add(panelGameNormField);
         buttonsGameNorm = new JButton[NORM_SIZE][NORM_SIZE];
+        int index = 0;
+        GameModel model = new GameModel();
+        model.setDiff(2);
+        model.createIcons();
+        model.iconsShuffle();
 
         for (int i = 0; i < NORM_SIZE; i++) {
             for (int j = 0; j < NORM_SIZE; j++) {
                 buttonsGameNorm[i][j] = new JButton();
+                buttonsGameNorm[i][j].setIcon(model.getIcon(index));
                 panelGameNormField.add(buttonsGameNorm[i][j]);
+                index++;
             }
         }
 
@@ -207,40 +215,48 @@ public class GameView extends JFrame {
         btnGameNormEnd.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 welcome();
-                getContentPane().removeAll();
-                getContentPane().add(panelWelcome);
-                getContentPane().revalidate();
-                getContentPane().repaint();
             }
         });
         btnGameNormEnd.setFont(new Font("Arial", Font.PLAIN, 15));
-        btnGameNormEnd.setBounds(517, 362, 105, 35);
+        btnGameNormEnd.setBounds(607, 515, 105, 35);
         panelGameNorm.add(btnGameNormEnd);
 
         btnGameNormNewStart = new JButton("Neustarten");
         btnGameNormNewStart.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
+                startNorm();
             }
         });
         btnGameNormNewStart.setFont(new Font("Arial", Font.PLAIN, 15));
-        btnGameNormNewStart.setBounds(389, 362, 105, 35);
+        btnGameNormNewStart.setBounds(479, 515, 105, 35);
         panelGameNorm.add(btnGameNormNewStart);
+
+        getContentPane().removeAll();
+        getContentPane().add(panelGameNorm);
+        getContentPane().revalidate();
+        getContentPane().repaint();
     }
 
     private void startHard() {
         panelGameHard = new JPanel();
         panelGameHardField = new JPanel();
-        panelGameHard.setBounds(0, 0, 634, 411);
-        panelGameHardField.setBounds(0, 1, 634, 350);
+        panelGameHard.setBounds(0, 0, 734, 561);
+        panelGameHardField.setBounds(0, 1, 734, 503);
         panelGameHard.setLayout(null);
         panelGameHardField.setLayout(new GridLayout(HARD_SIZE, HARD_SIZE));
         panelGameHard.add(panelGameHardField);
         buttonsGameHard = new JButton[HARD_SIZE][HARD_SIZE];
-
+        int index = 0;
+        GameModel model = new GameModel();
+        model.setDiff(3);
+        model.createIcons();
+        model.iconsShuffle();
         for (int i = 0; i < HARD_SIZE; i++) {
             for (int j = 0; j < HARD_SIZE; j++) {
                 buttonsGameHard[i][j] = new JButton();
+                buttonsGameHard[i][j].setIcon(model.getIcon(index));
                 panelGameHardField.add(buttonsGameHard[i][j]);
+                index++;
             }
         }
 
@@ -248,25 +264,26 @@ public class GameView extends JFrame {
         btnGameHardEnd.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 welcome();
-                getContentPane().removeAll();
-                getContentPane().add(panelWelcome);
-                getContentPane().revalidate();
-                getContentPane().repaint();
             }
         });
         btnGameHardEnd.setFont(new Font("Arial", Font.PLAIN, 15));
-        btnGameHardEnd.setBounds(517, 362, 105, 35);
+        btnGameHardEnd.setBounds(607, 515, 105, 35);
         panelGameHard.add(btnGameHardEnd);
 
         btnGameHardNewStart = new JButton("Neustarten");
         btnGameHardNewStart.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-
+                startHard();
             }
         });
         btnGameHardNewStart.setFont(new Font("Arial", Font.PLAIN, 15));
-        btnGameHardNewStart.setBounds(389, 362, 105, 35);
+        btnGameHardNewStart.setBounds(479, 515, 105, 35);
         panelGameHard.add(btnGameHardNewStart);
+
+        getContentPane().removeAll();
+        getContentPane().add(panelGameHard);
+        getContentPane().revalidate();
+        getContentPane().repaint();
     }
 
 }
